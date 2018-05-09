1af7052e8007a749ffe751532d54437824fbff91
Alex Marvick <alex@fiduciarytech.com>
2018-05-09 07:50:32 -0700

    Rewriting Server

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 8000;
+const server = require('http').Server(app);
 
 app.use(express.static(__dirname) + '/src');
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Console Listening on Port 8080');
-});
+// app.listen(process.env.PORT || 3000, function() {
+//     console.log('Console Listening on Port 8080');
+// });
+
+server.listen(port, function() {
+    console.log("App running on port " + port);
+})
 
 // PathLocationStrategy
 
