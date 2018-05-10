916f8d2a8fdb197c15ea6b8f7e581891f931c7a8
Alex Marvick <alex@fiduciarytech.com>
2018-05-09 20:14:26 -0700

    Favicon.ico

--- server.js
+++ server.js
@@ -6,15 +6,9 @@ const server = require('http').Server(app);
 
 app.use(express.static(__dirname + '/src'));
 
-// app.listen(process.env.PORT || 3000, function() {
-//     console.log('Console Listening on Port 8080');
-// });
-
 server.listen(port, function() {
     console.log("App running on port " + port);
 })
-
-// PathLocationStrategy
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/src/index.html'));
